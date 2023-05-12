1df6a2a89653db1f557d28a54b9a6f764f98c3e4
KimSound424 <ykj1512@gmail.com>
2023-05-12 14:18:45 +0900

    homework

--- BE/HomeWork/week2/blog/core/admin.py
+++ BE/HomeWork/week2/blog/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(TagPost)
+admin.site.register(Blog)
--- BE/HomeWork/week2/blog/core/models.py
+++ BE/HomeWork/week2/blog/core/models.py
@@ -1,16 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class User: 
-    user = User
-
 class Blog:
-    blogName = models.CharField(max_length=20, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    introdution = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
-        return self.blogNmae
+        return self.title
 
 class Category:
-    categoryName = models.CharField(max_length=20, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    
+class Tag:
+    name = models.CharField(max_length=20)
+    def __init__(self):
+        return self.name
+
+class Post:
+    title = models.CharField(max_length=200, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+class TagPost:
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class Comment:
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.comment
 
 # Create your models here.
