6d551d9325fe9b919b1d7ee4d1be6158e04a658c
KimSound424 <ykj1512@gmail.com>
2023-05-22 12:49:35 +0900

    init

---
+++ BE/HomeWork/week3/mypro/post/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Blog, Category, Post, Tag, TagPost, Comment
+# Register your models here.
+admin.site.register(Blog)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(TagPost)
+admin.site.register(Comment)
