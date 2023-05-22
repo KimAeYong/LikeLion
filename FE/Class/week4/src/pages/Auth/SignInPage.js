d688662b01ba2f09884a3f613b4dde5e8da3a7c8
KimSound424 <ykj1512@gmail.com>
2023-05-22 20:50:09 +0900

    login style

--- FE/Class/week4/src/pages/Auth/SignInPage.js
+++ FE/Class/week4/src/pages/Auth/SignInPage.js
@@ -1,22 +1,23 @@
 import { useNavigate } from 'react-router-dom';
 import { LogInForm } from 'components/Auth/index';
+import { SignInBox } from 'styles/SignContainer.styled';
 
 const SignInPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <SignInBox>
       <h3>로그인</h3>
 
       <LogInForm />
 
       <div className='bottom-signup'>
         <p>계정이 없으신가요?</p>
-        <button className='signupBtn' onClick={() => navigate('/signup')}>
+        <button className='signupBtn' onClick={() => navigate('/')}>
           계정만들기
         </button>
       </div>
-    </div>
+    </SignInBox>
   );
 };
 
