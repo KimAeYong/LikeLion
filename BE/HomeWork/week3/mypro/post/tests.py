53d9f01f1e0804068f77f3515c92c70d85e6b67b
KimSound424 <ykj1512@gmail.com>
2023-05-23 12:01:11 +0900

    error

--- BE/HomeWork/week3/mypro/post/tests.py
+++ BE/HomeWork/week3/mypro/post/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+# from django.test import TestCase
+from django.contrib.auth.models import User
+# from .models import Blog, Post, Category
 
-# Create your tests here.
+# class Test(TestCase):
+#     def test_init(self):
+#         self.user = User.objects.create_user(username='test', password='test')
+#         self.blog = Blog.objects.create(blogName='test', user=self.user)
+#         self.category = Category.objects.create(categoryName='test', blog=self.blog)
+#         self.post = Post.objects.create(title='test', body='test', category=self.blog)
+from django.test import TestCase
+from django.urls import reverse
+from .models import Post, Blog, Category, User
+
+class PostCRUDTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='test')
+        self.blog = Blog.objects.create(blogName='test', user=self.user)
+        self.category = Category.objects.create(categoryName='test', blog=self.blog)
+        self.post = Post.objects.create(title='test', body='test', category=self.blog)
+
+    def test_post_create(self):
+        response = self.client.post(reverse('post_create'), {
+            'title': 'New Post',
+            'body': 'This is a new post.',
+            'date': '2023-05-23',
+            'category': 'newCatagory'
+        })
+        self.assertEqual(response.status_code, 302)  # Check if the post was created successfully
+        self.assertEqual(Post.objects.count(), 2)  # Check if the number of posts increased by 1
+
+    def test_post_detail(self):
+        response = self.client.get(reverse('post_detail', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 200)  # Check if the post detail page is accessible
+        self.assertContains(response, self.post.title)  # Check if the post title is present in the response
+
+    def test_post_update(self):
+        response = self.client.post(reverse('post_update', args=[self.post.pk]), {
+            'title': 'Updated Post',
+            'body': 'This is an updated post.',
+            'date': '2023-05-23',
+            'category': 'Updated Category'
+        })
+        self.assertEqual(response.status_code, 302)  # Check if the post was updated successfully
+        self.post.refresh_from_db()
+        self.assertEqual(self.post.title, 'Updated Post')  # Check if the post title was updated
+
+    def test_post_delete(self):
+        response = self.client.post(reverse('post_delete', args=[self.post.pk]))
+        self.assertEqual(response.status_code, 302)  # Check if the post was deleted successfully
+        self.assertEqual(Post.objects.count(), 0)  # Check if the post was deleted from the database
--- BE/HomeWork/week3/mypro/post/views.py
+++ BE/HomeWork/week3/mypro/post/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    posts = Post.objects.all() 
+    paginator = Paginator(posts, 10)  
+    pageNum = request.GET.get('page') 
+    posts = paginator.get_page(pageNum)
+    return render(request, 'home', {'posts':posts})
+
+def post_create(request):
+    title = request.POST.get('title')
+    body = request.POST.get('body')
+    date = request.POST.get('date')
+    category = request.POST.get('category')
+    Post.objects.create(title=title, body=body, date=date, category=category)
+    return redirect(reverse('post_detail', args=[Post.pk]))
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post':post})
+    
+def post_update(request, pk):
+    if Post.pk.filter(pk=pk).exists():
+        post = get_object_or_404(Post, pk=pk)
+        post.title = request.POST.get('title')
+        post.body = request.POST.get('body')
+        post.date = request.POST.get('date')
+        post.category = request.POST.get('category')
+        post.save()
+        return redirect(reverse('post_detail'), args=[post.pk])
+    else:
+        return redirect(reverse('post_detail'), args=[post.pk])
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    messages.success(request, 'Post deleted successfully')
+    return redirect('home')
+
